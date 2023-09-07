03158a4b18bafe1abbb0c33d8a04e0f29f6853ea
YonasBenaddi <yobe0001@stud.kea.dk>
2023-09-07 15:20:22 +0200

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld" + " hello dat23d");
+        System.out.println("Dette er årstal: "+ 23+1);
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+        System.out.println(LocalTime.now());
+        LocalDateTime TodayTime = LocalDateTime.now();
+        System.out.println(2+2*10);
+        
+
+    }
+}
